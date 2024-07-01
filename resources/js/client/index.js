491d862d8acb4ba6fd3c1d52e595204f16a68194
Anggito Wicaksono <anggitov@gmail.com>
2024-07-01 17:51:08 +0700

    feat: add feature delete client

--- resources/js/client/index.js
+++ resources/js/client/index.js
@@ -37,19 +37,10 @@ $(document).ready(function () {
 
   $("#table-client").on("click", ".btn-delete", function() {
     const name = $(this).data("name");
-    const isActive = $(this).data("is-active");
-    const btnColor = $(this).data("btn-color");
     const url = $(this).data("url");
 
-    let status = 'activate';
-    if (isActive) {
-      status = 'deactive';
-    }
-
     $("#name").text(name);
-    $("#status").text(status);
     $("#delete").attr("href", url);
-    $("#delete").addClass(btnColor);
     $("#modal-toggle-activate").modal("show");
   });
 });
